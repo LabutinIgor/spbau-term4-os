924218450ed491c2928abae66e3b15fddce1eeeb
IgorL <labutin.igorl@gmail.com>
2016-04-05 15:56:00 +0300

    removed unused struct in lock.h

--- lock.h
+++ lock.h
@@ -3,9 +3,5 @@
 #include "threads.h"
 #include "interrupt.h"
 
-typedef struct lock_decsriptor {
-    int is_locked;
-} lock_decsriptor;
-
 void lock(int *descriptor);
 void unlock(int *descriptor);
